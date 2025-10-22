b266cc4ff416788bf91ae9c12ec7d8d440955f5f
mguibson <marcos.silva@softlive.dev>
2025-10-22 10:30:29 -0300

    Add initial project setup with theme and typography components
    - Created a new theme using Shopify Restyle with a defined color palette and spacing.
    - Added Box and Text components for layout and typography.
    - Updated App component to utilize the new theme and components.
    - Introduced .yarnrc.yml for Yarn configuration.
    - Added VSCode settings for ESLint integration.
    - Included multiple font files for Poppins and Satoshi families in the project.
    - Updated package.json to include new dependencies for Restyle and fonts.

--- App.tsx
+++ App.tsx
@@ -5,41 +5,24 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { ThemeProvider } from '@shopify/restyle';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { theme } from './src/theme/theme';
+import { Text } from './src/components/Text/Text';
+import { Box } from './src/components/Box/Box';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <Box>
+          <Text>Hello World</Text>
+        </Box >
+      </SafeAreaProvider >
+    </ThemeProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,64 @@
+import { createTheme } from '@shopify/restyle';
+
+export const palette = {
+  greenLight: '#DFF7E2',
+  greenHoneydew: '#F1FFF3',
+  greenPrimary: '#00D09E',
+  greenCyprus: '#0E3E3E',
+  greenDark: '#052224',
+
+  blueLight: '#6DB6FE',
+  blueVivid: '#3299FF',
+  blueOcean: '#0068FF',
+
+  redError: '#EA3838',
+  redErrorLight: '#FBECEC',
+
+  black: '#031314',
+  white: '#F0F2F3',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+    primary: palette.greenPrimary,
+    primaryContrast: palette.greenCyprus,
+    background: palette.greenHoneydew,
+    backgroundContrast: palette.greenDark,
+
+    success: palette.greenPrimary,
+    successLight: palette.greenLight,
+
+    error: palette.redError,
+    errorLight: palette.redErrorLight,
+  },
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s10: 10,
+    s12: 12,
+    s14: 14,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s32: 32,
+    s36: 36,
+    s40: 40,
+    s44: 44,
+    s48: 48,
+    s52: 52,
+    s56: 56,
+  },
+  borderRadii: {
+    s8: 8,
+    s12: 12,
+    s16: 16,
+    s20: 20,
+  },
+  textVariants: {
+    defaults: {},
+  },
+});
+
+export type Theme = typeof theme;
+export type ThemeColors = keyof Theme['colors'];
